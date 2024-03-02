db4a2acc24553e6ea05cec9f64756959dfb514a0
nikolayhotim1 <hotim_nikolay@mail.ru>
2024-03-02 14:34:56 +0300

    feature: sidebar

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,24 @@
+import { Navbar } from './_components/navbar'
+import { OrgSidebar } from './_components/org-sidebar'
+import { Sidebar } from './_components/sidebar'
+
+interface DashboardLaoyutProps {
+	children: React.ReactNode
+}
+
+export default function DashboardLaoyut({ children }: DashboardLaoyutProps) {
+	return (
+		<main className='h-full'>
+			<Sidebar />
+			<div className='pl-[60px] h-full'>
+				<div className='flex gap-x-3 h-full'>
+					<OrgSidebar />
+					<div className='h-full flex-1'>
+						<Navbar />
+						{children}
+					</div>
+				</div>
+			</div>
+		</main>
+	)
+}
