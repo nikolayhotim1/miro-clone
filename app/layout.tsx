221d180732600c4ff4f45dc6e96c3ca57ac1a410
nikolayhotim1 <hotim_nikolay@mail.ru>
2024-03-10 16:08:58 +0300

    fix: add suspense loading for app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import './globals.css'
 import { Toaster } from 'sonner'
 import { ConvexClientProvider } from '@/providers/convex-client-provider'
 import { ModalProvider } from '@/providers/modal-provider'
+import { Suspense } from 'react'
+import { Loading } from '@/components/auth/loading'
 
 const inter = Inter({ subsets: ['latin'] })
 interface LayoutProps {
@@ -19,11 +21,13 @@ export default function Layout({ children }: Readonly<LayoutProps>) {
 	return (
 		<html lang='en'>
 			<body className={inter.className}>
-				<ConvexClientProvider>
-					<Toaster />
-					<ModalProvider />
-					{children}
-				</ConvexClientProvider>
+				<Suspense fallback={<Loading />}>
+					<ConvexClientProvider>
+						<Toaster />
+						<ModalProvider />
+						{children}
+					</ConvexClientProvider>
+				</Suspense>
 			</body>
 		</html>
 	)
